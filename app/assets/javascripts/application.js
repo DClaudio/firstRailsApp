9a25fb24819af7ebffb6da6cf5b7b0ddcc38266f
DClaudio <david_claudio_t@yahoo.com>
2015-06-19 17:07:54 +0100

    completed ajax functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,52 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+
+var BusTimetable = function(){
+
+    var arrivalsUrl = "bus-timetable/arrivals";
+
+    function updateDomWithArrivals(busArrivals){
+        //remove table contents
+        $("#arrivals-body").empty()
+        //add new content to the table
+        var formatedContent = formatContent(busArrivals);
+        $("#arrivals-body").append(formatedContent);
+    }
+
+    function formatContent(busArrivals){
+        var content = "";
+        // for each syntax supported only in ES6
+        for(var index=0; index < busArrivals.length; index++){
+            content += "<tr>";
+            content += "<td>"+index+"</td>" +
+                    "<td>"+busArrivals[index].route+"</td>" +
+                    "<td>"+busArrivals[index].destination+"</td>" +
+                    "<td>"+busArrivals[index].timeToStation+"</td>";
+            content += "</tr>";
+        }
+        return content;
+    }
+
+    var getArrivalsAndUpdateDom = function(){
+        $.get( arrivalsUrl)
+            .done(function(jsonData) {
+                updateDomWithArrivals(jsonData);
+            })
+            .fail(function() {
+                alert( "Failed to update arrivals" );
+            })
+    }
+
+    return {updateArrivals: getArrivalsAndUpdateDom};
+
+}();
+
+
+$( document ).ready(function() {
+    $("button#update-arrivals").on( "click", function(){
+        BusTimetable.updateArrivals();
+    });
+});
